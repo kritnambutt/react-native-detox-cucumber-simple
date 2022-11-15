f6d196f9c83316aa01568c387d3498c8d5b186cc
KRIT NAMBUTT <toyota026265@gmail.com>
2022-11-15 15:57:11 +0700

    add helpers sleep and use it (for example)

--- e2e/step_definitions/common/given.js
+++ e2e/step_definitions/common/given.js
@@ -1,7 +1,9 @@
 import {Given} from '@cucumber/cucumber';
 import {expect, element, by} from 'detox';
+import {sleep} from '../../support';
 
 Given('I should see the {string} element', async elementId => {
+  await sleep(10000);
   await expect(element(by.id(elementId))).toBeVisible();
 });
 
